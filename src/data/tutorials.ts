81beb549015dc360cc3b8a9f0bdff3e5b43726fb
Ambrose Ahawo <ambroseahawo@gmail.com>
2025-06-09 00:52:33 +0300

    Update tutorials data

--- src/data/tutorials.ts
+++ src/data/tutorials.ts
@@ -1,26 +1,32 @@
 export interface Tutorial {
   title: string;
   description: string;
-  src: string;
+  coverImage: string;
   link: string;
   tags?: string[];
   featured: boolean;
+  publishedAt: string,
+  updatedAt: string,
 }
 
 export const tutorials: Tutorial[] = [
   {
     title: 'The Third Age of JavaScript',
     description: 'Solutions for running containers locally and remotely.',
-    src: '/images/popular-post-01.jpg',
+    coverImage: '/images/popular-post-01.jpg',
     link: '#0',
     tags: ['Open-Source'],
-    featured: true
+    featured: true,
+    publishedAt: '2024-03-20',
+    updatedAt: '2024-03-20'
   },
   {
     title: 'Building in Public Strategy',
     description: 'Solutions for running containers locally and remotely.',
-    src: '/images/popular-post-02.jpg',
+    coverImage: '/images/popular-post-02.jpg',
     link: '#0',
-    featured: true
+    featured: true,
+    publishedAt: '2024-03-20',
+    updatedAt: '2024-03-20'
   },
 ]; 
